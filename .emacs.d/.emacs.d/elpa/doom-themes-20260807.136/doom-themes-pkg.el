935bb221f1d39e524afbcee96423ba2722e6a59e
TheSleepster <ibjal20@outlook.com>
2026-09-20 20:31:44 -0500

    correct arrangement

---
+++ .emacs.d/.emacs.d/elpa/doom-themes-20260807.136/doom-themes-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "doom-themes" "20260807.136"
+  "An opinionated pack of modern color-themes."
+  '((emacs  "25.1")
+    (cl-lib "0.5"))
+  :url "https://github.com/doomemacs/themes"
+  :commit "a9b7144a4b6fe03be144b1610e0a6840e2b24d41"
+  :revdesc "a9b7144a4b6f"
+  :keywords '("themes" "faces")
+  :authors '(("Henrik Lissner" . "contact@henrik.io"))
+  :maintainers '(("Henrik Lissner" . "contact@henrik.io")))
--- stash.sh
+++ stash.sh
@@ -1,1 +1,1 @@
-stow alacritty bin fish ghostty gtk-3.0 gtk-4.0 hypr kdeglobals Kvantum mpd mpv nvim qt6ct quickshell rmpc rofi sddm Thunar tmux vid-cli wallpapers waybar .themes .icons
+stow -v alacritty bin fish ghostty gtk-3.0 gtk-4.0 hypr kdeglobals Kvantum mpd mpv nvim qt6ct quickshell rmpc rofi sddm Thunar tmux vid-cli wallpapers waybar .themes .icons
